13ba81d15069b4038c4c6e9bc5ab3b4e45d12f3a
Marco Barragan <mbarraga@thoughtworks.com>
2017-07-07 12:46:42 -0400

    Finished TODOs for 2-1

---
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.sunshine;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.android.sunshine.data.SunshinePreferences;
+import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+import com.example.android.sunshine.utilities.SunshineDateUtils;
+import com.example.android.sunshine.utilities.SunshineWeatherUtils;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mWeatherTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forecast);
+
+        /*
+         * Using findViewById, we get a reference to our TextView from xml. This allows us to
+         * do things like set the text of the TextView.
+         */
+        mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
+
+        loadWeatherData();
+    }
+
+    private void loadWeatherData() {
+        String userLocation = SunshinePreferences.getPreferredWeatherLocation(this);
+        URL url = NetworkUtils.buildUrl(userLocation);
+        SunshineAsyncTask task = new SunshineAsyncTask();
+        task.execute(url);
+    }
+
+    class SunshineAsyncTask extends AsyncTask<URL, Void, String>{
+        @Override
+        protected String doInBackground(URL... urls) {
+            String weatherResults = null;
+            try {
+                weatherResults = NetworkUtils.getResponseFromHttpUrl(urls[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return weatherResults;
+        }
+
+        @Override
+        protected void onPostExecute(String results) {
+            String [] weatherData = {};
+            if (results != null && !results.equals("")){
+                try {
+                    weatherData = OpenWeatherJsonUtils.getSimpleWeatherStringsFromJson(getApplicationContext(), results);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (weatherData != null && weatherData.length > 0){
+                for(String data: weatherData){
+                    mWeatherTextView.setText(data + "\n");
+                }
+            }
+        }
+    }
+}
