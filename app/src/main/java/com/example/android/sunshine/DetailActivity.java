1a4bdc5cc05d379f080dd868049cca0237c6997c
Marco Barragan <mbarraga@thoughtworks.com>
2017-07-10 12:08:32 -0400

    Finished TODOs for 4-3

---
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -0,0 +1,56 @@
+package com.example.android.sunshine;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    private String mForecast;
+    private TextView mWeatherDisplay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        mWeatherDisplay = (TextView) findViewById(R.id.tv_display_weather);
+
+        Intent intentThatStartedThisActivity = getIntent();
+
+        if (intentThatStartedThisActivity != null) {
+            if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
+                mForecast = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+                mWeatherDisplay.setText(mForecast);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.details, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_share){
+            String mime = "text/plain";
+            ShareCompat.IntentBuilder.from(this)
+                    .setText(mForecast)
+                    .setChooserTitle(getString(R.string.app_name))
+                    .setType(mime)
+                    .startChooser();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // TODO (3) Create a menu with an item with id of action_share
+    // TODO (4) Display the menu and implement the forecast sharing functionality
+}
